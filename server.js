68fe9d5a6d5daac54fb9a95aaff967b4af51d691
Santerne Chloé <chlosanterne@gmail.com>
2021-05-06 19:19:18 +0200

    version1.2

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 //Install express server
 const express = require('express');
+// Requirement of path Module in order to manage URLS
 const path = require('path');
-
 const app = express();
-
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/mes-petits-fanions'));
-
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/mes-petits-fanions/'}),
-);
-
+app.use(express.static(__dirname + '/dist/mes-petits-fanions'));
+app.get('/*', function(req,res) {
+   res.sendFile(path.join(__dirname+'/dist/mes-petits-fanions/src/index.html'));
+});
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
